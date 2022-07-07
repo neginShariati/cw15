cf33ea78f77ff08a42e1dbf208e90bc68817aea5
Zeynab Mousavi <zeynab11mousavi.com>
2022-07-07 16:54:16 +0430

    handle ceil floor round pow sqrt abc Zeynab mousavi

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
 const res = document.getElementById("res");
+let flagPow = false;
 
 function addCalculate(value) {
   res.value += value;
+  if(flagPow){
+    let powers = (res.value).split('^');
+    res.value= Math.pow(powers[0],powers[1]);
+    flagPow = false;
+
+  }
 }
-function calculate(){
-    res.value = eval(res.value);
+
+function calculate() {
+
+  res.value !== "" ? eval(res.value) : "";
+
 }
-function clearValue(){
-    res.value= "";
+
+function clearValue() {
+  res.value = "";
 }
-function mathToCalculate (){
-    
+
+function mathToCalculate(mathName) {
+
+  if(mathName=='pow'){
+    flagPow = true;
+    res.value += '^';
+  } else{
+
+    res.value = Math[mathName](res.value);
+  }
 }
+
